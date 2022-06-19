030800db0e585913f367b9da5fb99a1505424aa0
plusw <a2833772178@163.com>
2022-06-19 08:10:13 +0800

    hello

--- demo24_6.13课后练习定义cat和dog类/Main.java
+++ demo24_6.13课后练习定义cat和dog类/Main.java
@@ -1,6 +1,6 @@
 public class Main{
 	public static void main(String[] args){
-		Cat myCat=new Cat("黑色","波斯");
+		Cat myCat=new Cat("花色","波斯");
 		Dog myDog=new Dog("黑色","藏獒");
 		
 		myCat.eat();
---
+++ md/String.md
@@ -0,0 +1,2 @@
+#String 常用方法：
+String的toLowerCase方法，将字符串中的字符都转为小写。
